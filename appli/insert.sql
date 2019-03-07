12f9215c283d1a4f359b0a3ecd4112d57fdfb6f0
Antonin PANDION <antoninpandion28@gmail.com>
2019-03-07 17:17:54 +0100

    Changementé

---
+++ appli/insert.sql
@@ -0,0 +1,1 @@
+insert into JEU values("League of Legends", "iconeLoL.png", "imageLoL.jpg",'<iframe width="560" height="315" src="https://www.youtube.com/embed/8fwwGCr1-po" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',2009,"Jeu en arène Lux c'est un mage.","MOBA","Riot Games");
