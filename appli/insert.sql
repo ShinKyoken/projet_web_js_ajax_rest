f8688a1b04530097f084f3df48fec59ea022d0a9
Pandiaf <antoninpandion28@gmail.com>
2019-03-09 00:08:11 +0100

    Mise en forme du formulaire de l'ajout d'un jeu et d'un éditeur

--- appli/insert.sql
+++ appli/insert.sql
@@ -1,1 +1,1 @@
-insert into JEU values("League of Legends", "iconeLoL.png", "imageLoL.jpg",'<iframe width="560" height="315" src="https://www.youtube.com/embed/8fwwGCr1-po" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',2009,"Jeu en arène Lux c'est un mage.","MOBA","Riot Games");
+insert into JEU values("League of Legends", "iconeLoL.png", "imageLoL.jpg","https://www.youtube.com/embed/8fwwGCr1-po",2009,"Jeu en arène Lux c'est un mage.","MOBA","Riot Games");
